9be207151210fd4182f440985efc76bbf0361582
J0hnux <jc24.doliente@gmail.com>
2023-08-06 15:08:09 +0800

    replaced code with nextauth client page code

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {  
-  return (
-    <div className={'bg-gray-500 w-screen h-screen flex items-center'}>
-      <div className='text-center w-full'>
-        <button className='bg-white p-2 px-4 rounded-lg'>Login</button>
+  const { data: session } = useSession();
+
+  if(!session) {
+    return (
+      <div className={'bg-gray-500 w-screen h-screen flex items-center'}>
+        <div className='text-center w-full'>
+          <button onClick={() => signIn('google')} className='bg-white p-2 px-4 rounded-lg'>Login</button>
+        </div>
       </div>
-    </div>
+    );
+  }
+
+  return (
+    <div>Logged in as {session.user.name}</div>
   );
 }
