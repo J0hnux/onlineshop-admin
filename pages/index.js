b846c49b62a81a4434f9ab53c0187f543b8b2479
J0hnux <jc24.doliente@gmail.com>
2023-08-06 14:33:56 +0800

    Added Login text

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className={'bg-gray-500 w-screen h-screen flex items-center'}>
       <div className='text-center w-full'>
-        <button className='bg-white p-2 px-4 rounded-lg'>Login with Google</button>
+        <button className='bg-white p-2 px-4 rounded-lg'>Login</button>
       </div>
     </div>
   );
